0c7be9bf02eea529b68538799b2fab51d9da72b3
이태성 <tomleets@MacBook-Pro.local>
2020-08-07 16:42:58 +0900

    Add: mobility.views, mobility.test

--- mobility/models.py
+++ mobility/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db    import models
 
 from daily.models import State
 
@@ -17,7 +17,7 @@ class Place(models.Model):
 class Mobility(models.Model):
     state      = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
     place      = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True)
-    visit_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    visit_rate = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     is_deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

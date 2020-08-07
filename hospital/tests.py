973ab452784e753ff1cf79402a20e00702b2f81d
이태성 <tomleets@MacBook-Pro.local>
2020-08-07 16:27:16 +0900

    Add: hospital.views, hospital.test

--- hospital/tests.py
+++ hospital/tests.py
@@ -1,3 +1,83 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test  import (
+    TestCase,
+    Client
+)
+
+from daily.models import State
+from .models import (
+    Icu,
+    Bed
+)
+
+class BedTest(TestCase):
+    def setUp(self):
+        state = State.objects.create(
+            name = 'Alaska',
+            populations = 737438,
+            is_deleted = 0
+        )
+        Bed.objects.create(
+            state = state,
+            beds_per_thousand = 2.00,
+            is_deleted = 0
+        )
+
+    def tearDown(self):
+        State.objects.all().delete()
+        Bed.objects.all().delete()
+
+    def test_bedview_testcase_success(self):
+        
+        response = self.client.get('/bed')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'result': 'success',
+            "data": [
+                {
+                    "id": 'Alaska',
+                    'year': 2018,
+                    'value': '2.00'
+                }]})
+    def test_bedview_testcase_fail(self):
+
+
+        response = self.client.get('/beds')
+        self.assertEqual(response.status_code, 404)
+
+        
+class IcuTest(TestCase):
+    def setUp(self):
+        state = State.objects.create(
+            name = 'Alaska',
+            populations = 737438,
+            is_deleted = 0
+        )
+        Icu.objects.create(
+            state= state,
+            total = 217,
+            total_per_capita = 0.29,
+            is_deleted = 0
+        )
+
+    def tearDown(self):
+        State.objects.all().delete()
+        Icu.objects.all().delete()
+
+    def test_icuview_testcase_success(self):
+        response = self.client.get('/icu')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'result': 'success',
+            'data': [
+                {
+                    "id": "Alaska",
+                    "total": 217,
+                    "result": "0.29"
+                }]})
+
+
+    def test_icuview_testcase_fail(self):
+        response = self.client.get('/icus')
+        self.assertEqual(response.status_code, 404)
--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http  import JsonResponse
+from django.views import View
 
-# Create your views here.
+from .models      import (
+    Bed,
+    Icu
+)
+
+class BedView(View):
+    def get(self, request):
+        bed_data = Bed.objects.select_related('state')
+        data = [{
+            'id'   : item.state.name,
+            'year' : 2018,
+            'value': item.beds_per_thousand,
+        } for item in bed_data.all()]
+        return JsonResponse({'result': 'success', 'data': data})
+
+class IcuView(View):
+    def get(self, request):
+        icu_data = Icu.objects.select_related('state')
+        data = [{
+            'id'    : item.state.name,
+            'total' : item.total,
+            'result': item.total_per_capita
+        }for item in icu_data.all()]
+        return JsonResponse({'result': 'success', 'data': data})
